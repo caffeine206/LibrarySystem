92ca408ce0d355b4c98177d11d2b873804930e51
Derek Willms <caffeine206@gmail.com>
2014-03-02 13:32:53 -0800

    Added private variables and respective getter functions

--- Book.h
+++ Book.h
@@ -1,10 +1,22 @@
 #ifndef __Book_H
 #define __Book_H
 
-class Book {
+class Book : public Model {
   public:
+    getAuthor();
+    getTitle();
+    getCategoryID();
+    getDay();
+    getMonth();
+    getYear();
   
   protected:
+    string author;
+    string title;
+    int categoryID;
+    int day;
+    int month;
+    int year;
 };
 
 #endif
